d97a2efd03177eb7062236ba502bcf183007715b
juBo-uk <github@jubo.co.uk>
2013-10-04 23:54:06 +0100

    first commit

---
+++ start.php
@@ -0,0 +1,13 @@
+<?php
+elgg_register_event_handler('init', 'system', 'easytheme_init');
+
+function easytheme_init() {
+	        elgg_extend_view('css/elgg', 'easytheme/css');
+		      elgg_unregister_menu_item('topbar', 'elgg_logo');
+          elgg_extend_view('css/admin', 'easytheme/admin'); 
+          elgg_extend_view ('page/elements/head','easytheme/meta');
+
+		$base = elgg_get_plugins_path() . 'easytheme/actions/easytheme';
+		elgg_register_action('easytheme/settings/save', "$base/save.php", 'admin');
+
+}
